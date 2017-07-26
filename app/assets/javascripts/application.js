e22c8d1335f44f17144c57abb7bb9d333c8b439f
Saquib Anwar <saquib.anwar@icloud.com>
2017-07-26 13:24:53 +1200

    Reorder Javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
 
