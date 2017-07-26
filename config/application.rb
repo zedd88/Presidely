122c8ccbdff86a93c3eec7326a73a73efa520cca
Saquib Anwar <saquib.anwar@icloud.com>
2017-07-26 15:48:25 +1200

    Added Admin user

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Presidely
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.app_generators.scaffold_controller = :scaffold_controller
   end
 end
